577ba72b8a7cfa204da9f989c79a7c528656188f
Enric Tobella <enric.tobella@dixmit.com>
2023-11-13 15:12:50 +0100

    [ADD] l10n_es_aeat_mod322_special_prorate_regularization_capital_asset_legacy

---
+++ l10n_es_aeat_mod322_special_prorate_regularization_capital_asset_legacy/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=================================================================
+AEAT 322 - Special prorate regularization capital asset legacy
+=================================================================
+
+* This module extends Special Prorate Regularization Capital Asset to include old assets.
---
+++ l10n_es_aeat_mod322_special_prorate_regularization_capital_asset_legacy/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright Dixmit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "AEAT 322 - Special Prorate Regularization Capital Asset legacy",
+    "summary": "This module extends Special Prorate Regularization Capital Asset "
+    "to include old assets",
+    "version": "14.0.1.0.1",
+    "category": "Accounting",
+    "author": "Dixmit, Oxigen Salud",
+    "website": "https://github.com/oxigensalud/odoo-community-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_mod322_special_prorate_regularization_capital_asset",
+    ],
+    "data": [
+        "views/aeat_tax_line_view.xml",
+    ],
+}
