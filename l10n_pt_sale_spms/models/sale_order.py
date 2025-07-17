fcaae52f9efaaf364ffa7e381f99d145befb2917
??? <dgallo@nuobit.com>
2025-07-17 16:32:20 +0200

    [ADD] l10n_pt_sale_spms

---
+++ l10n_pt_sale_spms/models/sale_order.py
@@ -0,0 +1,10 @@
+# Copyright 2025 NuoBiT - Deniz Gallo <dgallo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    spms_information = fields.Boolean(related="partner_id.spms_information")
