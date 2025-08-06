6485fc1ee4437bb9b6902fd972bf65cdc1a077da
Enric Tobella <enric.tobella@dixmit.com>
2025-08-06 15:19:43 +0200

    [IMP] l10n_pt_spms: Add SPMS Information field

---
+++ l10n_pt_spms/models/res_partner.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+
+    _inherit = "res.partner"
+
+    spms_information = fields.Boolean()
